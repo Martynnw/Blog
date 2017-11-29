7b6ffff964b5e86fc2249544db5c9b1a3558593c
Martyn Wiggins <Martynnw@gmail.com>
2017-11-29 07:00:27 +0000

    Initial demo complete

---
+++ AutomatedStateTesting/AutomatedStateTesting/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AutomatedStateTesting
+{
+    internal class MainPageViewModel : MvvmHelpers.BaseViewModel
+    {
+        private string text;
+        private int clickCount;
+
+        public MainPageViewModel()
+        {
+            ClickCommand = new Command(IncrementClickCount);
+        }
+
+        public ICommand ClickCommand { get; private set; }
+
+        public string Text 
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+
+        public int ClickCount
+        {
+            get { return clickCount; }
+            set { SetProperty(ref clickCount, value); }
+        }
+
+        public string ClickCountText
+        {
+            get { return $"Clicked {ClickCount} Times"; }
+        }
+
+        private void IncrementClickCount()
+        {
+            ClickCount++;
+            OnPropertyChanged("ClickCountText");
+        }
+    }
+}
