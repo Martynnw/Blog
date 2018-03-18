7442c9f5c783ddb54293f438499215463ef257e6
Martyn Wiggins <Martynnw@gmail.com>
2018-03-18 13:14:08 +0000

    Demo app working

---
+++ SpecFlowDemo/SpecFlowDemo/LoginPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SpecFlowDemo
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            BindingContext = new LoginViewModel();
+        }
+    }
+}
---
+++ SpecFlowDemo/SpecFlowDemo/LoginViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SpecFlowDemo
+{
+    public class LoginViewModel : MvvmHelpers.BaseViewModel
+    {
+        private string username;
+        private string password;
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await ExecuteLoginAsync());
+        }
+
+        public ICommand LoginCommand { get; private set; }
+
+        public string Username
+        {
+            get { return username; }
+            set { SetProperty(ref username, value); }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+
+        private async Task ExecuteLoginAsync()
+        {
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            if (CheckUserIsValid())
+            {
+                await ShowMainPage();
+            }
+            else
+            {
+                await ShowInvalidLogon();
+            }
+
+            IsBusy = false;
+        }
+
+        private bool CheckUserIsValid()
+        {
+            return Username.Equals("Olivia", StringComparison.CurrentCultureIgnoreCase) && Password.Equals("IHeartOwls");
+        }
+
+        private async Task ShowMainPage()
+        {
+            var mainPage = new MainPage();
+            await Application.Current.MainPage.Navigation.PushAsync(mainPage);
+        }
+
+        private async Task ShowInvalidLogon()
+        {
+            var title = "Logon Failed";
+            var text = "Invalid username or password";
+            await Application.Current.MainPage.DisplayAlert(title, text, "OK");
+        }
+    }
+}
